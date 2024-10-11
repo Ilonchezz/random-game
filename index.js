fca8d24cae814c50a9c4d4fdba0718f75c07dfb0
Ilona <zaitsavailona@gmail.com>
2024-10-12 00:39:14 +0300

    feat: add closed cells, add flags

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ for (let i = 0; i < height; i++) {
     field.push([]);
     for (let j = 0; j < width; j++) {
         field[i].push({
-            isMine: false
+            isMine: false,
+            minesAround: 0,
+            isOpened: false,
+            isFlagged: false 
         });
     }
 }
@@ -40,6 +43,38 @@ const getMineCount = (i, j) => {
     return field[i][j].isMine ? 1 : 0;
 }
 
+const handleClick = (i, j) => {
+    if (i < 0 || i >= height) {
+        return;
+    }
+    if (j < 0 || j >= width) {
+        return;
+    }
+    if (field[i][j].isOpened) {
+        return;
+    }
+    if (field[i][j].isFlagged) {
+        return;
+    }
+
+    field[i][j].isOpened = true;
+    if (field[i][j].isMine) {
+        return;
+    }
+
+    if (field[i][j].minesAround === 0) {
+        handleClick(i - 1, j - 1);
+        handleClick(i - 1, j);
+        handleClick(i - 1, j + 1);
+        handleClick(i, j - 1);
+        handleClick(i, j + 1);
+        handleClick(i + 1, j - 1);
+        handleClick(i + 1, j);
+        handleClick(i + 1, j + 1);
+    } 
+
+}
+
 for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
         field[i][j].minesAround = getMineCount(i - 1, j - 1) + getMineCount(i - 1, j) + getMineCount(i - 1, j + 1) +
@@ -48,15 +83,35 @@ for (let i = 0; i < height; i++) {
     }
 }
 
-for (let i = 0; i < height; i++) {
-    for (let j = 0; j < width; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        if (field[i][j].isMine) {
-            cell.innerHTML = '💣';
-        } else if(field[i][j].minesAround > 0) {
-            cell.innerHTML = field[i][j].minesAround.toString();
+const draw = () => {
+    container.innerHTML = '';
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            if (field[i][j].isOpened) {
+                if (field[i][j].isMine) {
+                    cell.innerHTML = '💣';
+                } else if(field[i][j].minesAround > 0) {
+                    cell.innerHTML = field[i][j].minesAround.toString();
+                }
+            } else if (field[i][j].isFlagged) {
+                cell.innerHTML = '🚩';
+            } else {
+                cell.classList.add('closed_cell');
+            }
+            cell.onclick = () => {
+                handleClick(i, j);
+                draw();
+            } 
+            cell.oncontextmenu = (e) => {
+                e.preventDefault();
+                field[i][j].isFlagged = !field[i][j].isFlagged;
+                draw();
+            }
+            container.appendChild(cell);
         }
-        container.appendChild(cell);
-    }
-}
+    }    
+};
+
+draw();
