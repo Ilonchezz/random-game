c9cd45b6b0a4dea040aeb1edb57c7b4a80117273
Ilona <zaitsavailona@gmail.com>
2024-10-14 01:28:30 +0300

    feat: add settings for game

--- index.js
+++ index.js
@@ -1,53 +1,113 @@
-const height = 9;
-const width = 9;
-const mines = 10;
-const field = [];
-
-for (let i = 0; i < height; i++) {
-    field.push([]);
-    for (let j = 0; j < width; j++) {
-        field[i].push({
-            isMine: false,
-            minesAround: 0,
-            isOpened: false,
-            isFlagged: false 
-        });
-    }
-}
-console.log(field);
-
-let mineIndexes = new Set();
-while (mineIndexes.size < mines) {
-   mineIndexes.add(Math.floor(Math.random() * (height * width))); 
-}
-console.log(mineIndexes);
-
-mineIndexes = Array.from(mineIndexes);
-for (let i = 0; i < mineIndexes.length; i++) {
-    field[Math.floor(mineIndexes[i] / width)][mineIndexes[i] % width].isMine = true;
-}
-console.log(field);
-
+let settings = {
+  height: 8,
+  width: 8,
+  mines: 10
+};
+ 
+let field = [];
+let isFieldLocked = false;
 const container = document.getElementById('container');
+const settingsBtn = document.getElementById('settings-btn');
+const closePopupBtn = document.getElementById('close-popup');
+const settingsPopup = document.getElementById('settings-popup');
+const newGame = document.getElementById('new-game-btn');
 
-container.style.gridTemplateColumns = `repeat(${width}, 30px)`;
-container.style.gridTemplateRows = `repeat(${height}, 30px)`;
+newGame.addEventListener('click', () => {
+    initialize();
+    draw();
+});
 
-const getMineCount = (i, j) => {
-    if (i < 0 || i >= height) {
-        return 0;
+settingsBtn.addEventListener('click', () => {
+    settingsPopup.classList.remove('hidden');
+});
+
+closePopupBtn.addEventListener('click', () => {
+    settingsPopup.classList.add('hidden');
+});
+
+document.getElementById('save-settings').addEventListener('click', () => {
+    const selectedDifficulty = document.querySelector('input[name="difficulty"]:checked').value;
+    
+    if (selectedDifficulty === 'beginner') {
+        settings = {
+            height: 8,
+            width: 8,
+            mines: 10
+        };
+    } else if (selectedDifficulty === 'intermediate') {
+        settings = {
+            height: 16,
+            width: 16,
+            mines: 40
+        };
+    } else if (selectedDifficulty === 'expert') {
+        settings = {
+            height: 16,
+            width: 30,
+            mines: 99
+        };
     }
-    if (j < 0 || j >= width) {
-        return 0;
+    
+    settingsPopup.classList.add('hidden');
+    initialize();
+    draw();
+});
+
+const initialize = () => {
+    field = [];
+    isFieldLocked = false;
+    for (let i = 0; i < settings.height; i++) {
+        field.push([]);
+        for (let j = 0; j < settings.width; j++) {
+            field[i].push({
+                isMine: false,
+                minesAround: 0,
+                isOpened: false,
+                isFlagged: false 
+            });
+        }
     }
-    return field[i][j].isMine ? 1 : 0;
+    console.log(field);
+    
+    let mineIndexes = new Set();
+    while (mineIndexes.size < settings.mines) {
+       mineIndexes.add(Math.floor(Math.random() * (settings.height * settings.width))); 
+    }
+    console.log(mineIndexes);
+    
+    mineIndexes = Array.from(mineIndexes);
+    for (let i = 0; i < mineIndexes.length; i++) {
+        field[Math.floor(mineIndexes[i] / settings.width)][mineIndexes[i] % settings.width].isMine = true;
+    }
+    console.log(field);
+
+    container.style.gridTemplateColumns = `repeat(${settings.width}, 30px)`;
+    container.style.gridTemplateRows = `repeat(${settings.height}, 30px)`;
+
+    const getMineCount = (i, j) => {
+        if (i < 0 || i >= settings.height) {
+            return 0;
+        }
+        if (j < 0 || j >= settings.width) {
+            return 0;
+        }
+        return field[i][j].isMine ? 1 : 0;
+    }
+
+    for (let i = 0; i < settings.height; i++) {
+        for (let j = 0; j < settings.width; j++) {
+            field[i][j].minesAround = getMineCount(i - 1, j - 1) + getMineCount(i - 1, j) + getMineCount(i - 1, j + 1) +
+              getMineCount(i, j - 1) + getMineCount (i, j + 1) + getMineCount(i + 1, j - 1) + getMineCount(i + 1, j) +
+              getMineCount(i + 1, j + 1);
+        }
+    }
 }
 
 const handleClick = (i, j) => {
-    if (i < 0 || i >= height) {
+    if (i < 0 || i >= settings.height) {
         return;
     }
-    if (j < 0 || j >= width) {
+    if (j < 0 || j >= settings.width) {
         return;
     }
     if (field[i][j].isOpened) {
@@ -59,6 +119,14 @@ const handleClick = (i, j) => {
 
     field[i][j].isOpened = true;
     if (field[i][j].isMine) {
+        isFieldLocked = true;
+        for (let a = 0; a < settings.height; a++) {
+            for (let b = 0; b < settings.width; b++) {
+                if (field[a][b].isMine) {
+                    field[a][b].isOpened = true;
+                }
+            }
+        }
         return;
     }
 
@@ -75,18 +143,12 @@ const handleClick = (i, j) => {
 
 }
 
-for (let i = 0; i < height; i++) {
-    for (let j = 0; j < width; j++) {
-        field[i][j].minesAround = getMineCount(i - 1, j - 1) + getMineCount(i - 1, j) + getMineCount(i - 1, j + 1) +
-          getMineCount(i, j - 1) + getMineCount (i, j + 1) + getMineCount(i + 1, j - 1) + getMineCount(i + 1, j) +
-          getMineCount(i + 1, j + 1);
-    }
-}
+
 
 const draw = () => {
     container.innerHTML = '';
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
+    for (let i = 0; i < settings.height; i++) {
+        for (let j = 0; j < settings.width; j++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             if (field[i][j].isOpened) {
@@ -102,11 +164,17 @@ const draw = () => {
                 cell.classList.add('closed_cell');
             }
             cell.onclick = () => {
+                if (isFieldLocked) {
+                    return;
+                }
                 handleClick(i, j);
                 draw();
             } 
             cell.oncontextmenu = (e) => {
                 e.preventDefault();
+                if (isFieldLocked) {
+                    return;
+                }
                 field[i][j].isFlagged = !field[i][j].isFlagged;
                 draw();
             }
@@ -114,5 +182,7 @@ const draw = () => {
         }
     }    
 };
-
+initialize();
 draw();
+
+   
